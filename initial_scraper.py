2b8dd3e939657c80e86740a707f614c70efc71f9
Exceed IO Inc <arthurlee087@gmail.com>
2022-04-22 11:45:40 -0500

    Update initial_scraper.py

--- initial_scraper.py
+++ initial_scraper.py
@@ -1,5 +1,5 @@
 import datetime
-
+import emailer
 from selenium.webdriver.common.by import By
 
 import main
@@ -9,7 +9,7 @@ driver = selenium.webdriver.Chrome()
 
 main_dict = {"base_url": "https://www.texaslottery.com/export/sites/lottery/Games/Scratch_Offs/index.html#",
              "price_points": [1, 2, 3, 5, 10, 20, 30, 50],
-             "ticket_info": []}
+             "ticket_info": {}}
 
 
 def OpenLotteryHomePage(price_point):  # in dollars, find the lottery page to extract info
@@ -52,72 +52,89 @@ def findDBClusters(full_string):
 #  print(GET_ALL_TICKET_URLS())
 def getTicketDetails(urls: list):
     for url in urls:
-        try:
-            driver.get(url)
-            time.sleep(.1)
-            page_src = driver.page_source
-            findDBClusters(full_string=page_src)
-            game_number = main.FIND_ALL_SUBSTRINGS_BETWEEN_2_STRINGS(full_string=page_src, p1="Game No. ", p2=" - ")[0]
 
-            game_name = driver.find_element(By.XPATH, "//*[@id=\"content\"]/div/div/div[1]/div/div/h2").text.split("- ")[1]
+        driver.get(url)
+        time.sleep(.1)
+        page_src = driver.page_source
+        findDBClusters(full_string=page_src)
+        game_number = main.FIND_ALL_SUBSTRINGS_BETWEEN_2_STRINGS(full_string=page_src, p1="Game No. ", p2=" - ")[0]
 
-            total_prizes_dollars = int(float(driver.find_element(By.XPATH, "//*[@id=\"content\"]/div/div/div[2]/div[2]/div/ul").text.split("$")[1].split(" ")[0])*1000000)
+        game_name = driver.find_element(By.XPATH, "//*[@id=\"content\"]/div/div/div[1]/div/div/h2").text.split("- ")[1]
 
-            pack_size = main.FIND_ALL_SUBSTRINGS_BETWEEN_2_STRINGS(full_string=page_src, p1="Pack Size: ", p2=" tickets")[0]
-            max_tickets = driver.find_element(By.XPATH, "//*[@id=\"content\"]/div/div/div[2]/div[2]/p[2]").text.split("approximately ")[1].split("*")[0].replace(",", "")
-            guaranteed_per_pack_usd = page_src.split("Amount = $")[1].split(" per pack")[0]
-            guaranteed_per_ticket_usd = int(pack_size)/float(guaranteed_per_pack_usd)
+        total_prizes_dollars = int(float(driver.find_element(By.XPATH, "//*[@id=\"content\"]/div/div/div[2]/div[2]/div/ul").text.split("$")[1].split(" ")[0])*1000000)
 
-            odds2 = main.SANDWHICHED_SUBSTRING(full_string=page_src, slice_1="1 in ", slice_2="**")
-            adjusted_odds = float(1.0/float(odds2))
-            temp_full_string = main.list_SANDWHICHED_SUBSTRING(full_string=page_src, slice_1="<tbody>", slice_2="</tbody>")
-            temp_array = []
-            for item in temp_full_string:
-                temp_array.append(item)
-            highest_prize = temp_array[0]
-            prize_data = temp_array
-            claimed_ratio_data_points = []  # will average them
-            available_ratio_data_points = []
-            for i in range(1, len(prize_data)-1, 3):
-                if i not in [6]:
-                    available_tickets = str(prize_data[i]).replace(",", "").replace("$", "")
-                    available_tickets = int(available_tickets)
-                    available_ratio_data_points.append(available_tickets)
-            for i in range(2, len(prize_data), 3):
-                claimed_tickets = str(prize_data[i]).replace(",", "").replace("$", "")
-                claimed_tickets = int(claimed_tickets)
-                claimed_ratio_data_points.append(claimed_tickets)
-            avg_claimed_ratio = float(sum(claimed_ratio_data_points)/sum(available_ratio_data_points))
-            avg_availability_chance = 1 - avg_claimed_ratio
-            table_cells_prizes = temp_array
-            #  print({"Table Cells": table_cells_prizes})
-            # TO DO GAME NUMBER, GAME ODDS, PACK SIZE, TOTAL PRIZES, LARGEST_PRIZE, MEAN_PRIZE, PACK
-            print(f"Game Number: {game_number}")
-            print(f"Game Name: {game_name}")
-            print(f"Total Prize Money Pool: ${total_prizes_dollars}")
-            print(f"Average ticket winnings: ${round(guaranteed_per_ticket_usd, 5)}")
-            print(f"Total Possible Tickets: {max_tickets}")
-            print(f"Adjusted Odds: 1 in {odds2}")
-            print(f"Highest Prize: {highest_prize}")
-            print(f"Current Prize Availability: {round(avg_availability_chance, 4)}%")
-            print(f"Pack Size: {pack_size} Tickets")
-            print("\n")
-            main_dict['ticket_info'].append({"Game Name": game_name,
-                                             "Game Number": game_number,
-                                             "Prize Pool": total_prizes_dollars,
-                                             "Average Winnings Per Ticket": guaranteed_per_ticket_usd,
-                                             "Tickets In Circulation": max_tickets,
-                                             "Adjusted Odds": adjusted_odds,
-                                             "Highest Prize": int(str(highest_prize).replace("$", "").replace(",", "")),
-                                             "Current Prize Availability": avg_availability_chance,
-                                             "Pack Size": int(pack_size),
-                                             })
-        except:
-            pass
+        pack_size = main.FIND_ALL_SUBSTRINGS_BETWEEN_2_STRINGS(full_string=page_src, p1="Pack Size: ", p2=" tickets")[0]
+        max_tickets = driver.find_element(By.XPATH, "//*[@id=\"content\"]/div/div/div[2]/div[2]/p[2]").text.split("approximately ")[1].split("*")[0].replace(",", "")
+        guaranteed_per_pack_usd = page_src.split("Amount = $")[1].split(" per pack")[0]
+        guaranteed_per_ticket_usd = float(guaranteed_per_pack_usd) / float(int(pack_size))
+        # print(page_src)
+        odds2 = main.SANDWHICHED_SUBSTRING(full_string=page_src, slice_1="1 in ", slice_2="**")
+        adjusted_odds = float(1.0/float(odds2))
+        temp_full_string = main.list_SANDWHICHED_SUBSTRING(full_string=page_src, slice_1="<tbody>", slice_2="</tbody>")
+        temp_array = []
+        for item in temp_full_string:
+            temp_array.append(item)
+        highest_prize = temp_array[0]
+        lowest_prize = temp_array[len(highest_prize)-3]
+        prize_data = temp_array
+        claimed_ratio_data_points = []  # will average them
+        available_ratio_data_points = []
+        for i in range(1, len(prize_data)-1, 3):
+            if i not in [6]:
+                available_tickets = str(prize_data[i]).replace(",", "").replace("$", "")
+                available_tickets = int(available_tickets)
+                available_ratio_data_points.append(available_tickets)
+        for i in range(2, len(prize_data), 3):
+            claimed_tickets = str(prize_data[i]).replace(",", "").replace("$", "")
+            claimed_tickets = int(claimed_tickets)
+            claimed_ratio_data_points.append(claimed_tickets)
+        avg_claimed_ratio = float(sum(claimed_ratio_data_points)/sum(available_ratio_data_points))
+        avg_availability_chance = (1 - avg_claimed_ratio) * 100
+        table_cells_prizes = temp_array
+        #  print({"Table Cells": table_cells_prizes})
+        # TO DO GAME NUMBER, GAME ODDS, PACK SIZE, TOTAL PRIZES, LARGEST_PRIZE, MEAN_PRIZE, PACK
+
+        #print(f"Price Per Ticket: {lowest_prize}")
+        print(f"Game Number: {game_number}")
+        print(f"Game Name: {game_name}")
+        print(f"Total Prize Money Pool: ${total_prizes_dollars}")
+        print(f"Average ticket winnings: ${round(guaranteed_per_ticket_usd, 5)}")
+        print(f"Ticket Price: ${lowest_prize}")
+        print(f"Total Possible Tickets: {max_tickets}")
+        print(f"Adjusted Odds: 1 in {odds2}")
+        print(f"Highest Prize: {highest_prize}")
+        print(f"Current Prize Availability: {round(avg_availability_chance, 4)}%")
+        print(f"Pack Size: {pack_size} Tickets")
+        base_url = "https://www.texaslottery.com/"
+        prefix_url_mod = "/export/sites/lottery/Images/scratchoffs/"
+        suffix_url_mid = f"{game_number}_img1.gif"
+        url1 = f"{base_url}{prefix_url_mod}{suffix_url_mid}"
+        suffix_url_mid = f"{game_number}_img2.gif"
+        url2 = f"{base_url}{prefix_url_mod}{suffix_url_mid}"
+        image_urls = [url1, url2]
+        # image_urls = image_urls.append(main.FIND_ALL_SUBSTRINGS_BETWEEN_2_STRINGS(page_src, "<img src=", '"')[0])
+        print({"Image URLS": image_urls})
+        print("\n")
+        main_dict['ticket_info'].update({game_number: {"Game Name": game_name,
+                                         "Game Number": game_number,
+                                         "Prize Pool": total_prizes_dollars,
+                                         "Average Winnings Per Ticket": guaranteed_per_ticket_usd,
+                                         "Tickets In Circulation": max_tickets,
+                                         "Adjusted Odds": adjusted_odds,
+                                         "Pack Value": f"${guaranteed_per_pack_usd}",
+                                         "Highest Prize": int(str(highest_prize).replace("$", "").replace(",", "")),
+                                         "Current Prize Availability": avg_availability_chance,
+                                         "Pack Size": int(pack_size),
+                                         'Ticket Price USD': float(lowest_prize.replace("$", "")),
+                                         "Image URLS": image_urls
+                                         }})
+        print(main_dict)
+        print("\n")
+
 
 
 print("\n")
 getTicketDetails(GET_ALL_TICKET_URLS())
-main.save_to_file(f"Scraped_Texas_Lottery_{datetime.date.today()}")
-
+main.save_to_file(file_name=f"Scraped_Texas_Lottery_{datetime.date.today()}.txt", final_string=str(main_dict))
+emailer.itoven_send_email_str(to="arthur@itoven-ai.co", subject=f"Lottery AI Report {datetime.date.today()}", message=str(main_dict))
 # TODO ACCESS EACH URL WE FOUND AND GET ODDS AND ADD TO JSON OR DICT WITH NAME OF TICKET AND URL TO TICKET
